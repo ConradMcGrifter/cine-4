55c30f7ea98f9b1c6500af7c5ae953a10c274230
skf <79630413+ConradMcGrifter@users.noreply.github.com>
2022-02-09 03:29:18 -0500

    update

--- js/schedule.js
+++ js/schedule.js
@@ -1,4 +1,37 @@
 export let schedule = {
+    movie4: {
+        title: "Licorice Pizza",
+        synopsis:
+            "Alana Kane and Gary Valentine grow up, run around and fall in love in California's San Fernando Valley in the 1970s.",
+        poster: "posters/licoricePizza.jpg",
+        trailer: "https://www.youtube.com/embed/ofnXPwUPENo?autoplay=1",
+        showtimes: {
+            mon: [""],
+            tue: ["7:30pm"],
+            wed: [""],
+            thu: ["7:30pm"],
+            fri: ["7:30pm"],
+            sat: ["12:30pm", "5:10pm"],
+            sun: ["12:30pm", "5:10pm"],
+        },
+    },
+
+    movie5: {
+        title: "West Side Story",
+        synopsis:
+            "Love at first sight strikes when young Tony spots Maria at a high school dance in 1957 New York City. Their burgeoning romance helps to fuel the fire between the warring Jets and Sharks -- two rival gangs vying for control of the streets.",
+        poster: "posters/WSSposter.jpg",
+        trailer: "https://www.youtube.com/embed/A5GJLwWiYSg?autoplay=1",
+        showtimes: {
+            mon: ["7:30pm"],
+            tue: [""],
+            wed: ["7:30pm"],
+            thu: [""],
+            fri: ["5:10pm"],
+            sat: ["2:30pm", "7:30pm"],
+            sun: ["2:30pm", "7:30pm"],
+        },
+    },
     movie1: {
         title: "Belfast",
         synopsis:
@@ -45,39 +78,6 @@ export let schedule = {
             fri: ["5:10pm", "7:30pm"],
             sat: ["12:30pm", "2:30pm", "5:10pm", "7:30pm"],
             sun: ["12:30pm", "2:30pm", "5:10pm", "7:30pm"],
-        },
-    },
-    movie4: {
-        title: "Licorice Pizza",
-        synopsis:
-            "Alana Kane and Gary Valentine grow up, run around and fall in love in California's San Fernando Valley in the 1970s.",
-        poster: "posters/licoricePizza.jpg",
-        trailer: "https://www.youtube.com/embed/ofnXPwUPENo?autoplay=1",
-        showtimes: {
-            mon: [""],
-            tue: ["7:30pm"],
-            wed: [""],
-            thu: ["7:30pm"],
-            fri: ["7:30pm"],
-            sat: ["12:30pm", "5:10pm"],
-            sun: ["12:30pm", "5:10pm"],
-        },
-    },
-
-    movie5: {
-        title: "West Side Story",
-        synopsis:
-            "Love at first sight strikes when young Tony spots Maria at a high school dance in 1957 New York City. Their burgeoning romance helps to fuel the fire between the warring Jets and Sharks -- two rival gangs vying for control of the streets.",
-        poster: "posters/WSSposter.jpg",
-        trailer: "https://www.youtube.com/embed/A5GJLwWiYSg?autoplay=1",
-        showtimes: {
-            mon: ["7:30pm"],
-            tue: [""],
-            wed: ["7:30pm"],
-            thu: [""],
-            fri: ["5:10pm"],
-            sat: ["2:30pm", "7:30pm"],
-            sun: ["2:30pm", "7:30pm"],
         },
     },
 };
