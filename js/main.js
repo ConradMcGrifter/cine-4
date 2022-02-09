03faba33483c3e9db854e8b0e7c4f25be1ddadfd
skf <79630413+ConradMcGrifter@users.noreply.github.com>
2022-02-08 21:07:21 -0500

    made the js modular

--- js/main.js
+++ js/main.js
@@ -1,89 +1,9 @@
+import { schedule } from "/js/schedule.js";
+
 let d = new Date();
 let currentDay = d.getDay();
-
-let schedule = {
-    movie1: {
-        title: "Belfast",
-        synopsis:
-            "BELFAST is a movie straight from Branagh's own experience. A nine-year-old boy must chart a path towards adulthood through a world that has suddenly turned upside down. His stable and loving community and everything he thought he understood about life is changed forever but joy, laughter, music and the formative magic of the movies remain.",
-        poster: "posters/belfastPoster.jpg",
-        trailer: undefined,
-        showtimes: {
-            mon: ["7:30pm"],
-            tue: ["7:30pm"],
-            wed: ["7:30pm"],
-            thu: ["7:30pm"],
-            fri: ["5:10pm", "7:30pm"],
-            sat: ["12:30pm", "2:30pm", "5:10pm", "7:30pm"],
-            sun: ["12:30pm", "2:30pm", "5:10pm", "7:30pm"],
-        },
-    },
-    movie2: {
-        title: "House of Gucci",
-        synopsis:
-            "House of Gucci is inspired by the shocking true story of the family behind the Italian fashion empire. When Patrizia Reggiani (Lady Gaga), an outsider from humble beginnings, marries into the Gucci family, her unbridled ambition begins to unravel the family legacy and triggers a reckless spiral of betrayal, decadence, revenge, and ultimately... murder.",
-        poster: "posters/gucciPoster.jpg",
-        trailer: "https://www.youtube.com/watch?v=pGi3Bgn7U5U",
-        showtimes: {
-            mon: ["7:30pm"],
-            tue: ["7:30pm"],
-            wed: ["7:30pm"],
-            thu: ["7:30pm"],
-            fri: ["5:10pm", "7:30pm"],
-            sat: ["12:30pm", "2:30pm", "5:10pm", "7:30pm"],
-            sun: ["12:30pm", "2:30pm", "5:10pm", "7:30pm"],
-        },
-    },
-    movie3: {
-        title: "Parallel Mothers",
-        synopsis:
-            "Two women, Janis and Ana, coincide in a hospital room where they are going to give birth. Both are single and became pregnant by accident. Janis, middle-aged, doesn't regret it and she is exultant. The other, Ana, an adolescent, is scared, repentant and traumatized. Janis tries to encourage her while they move like sleepwalkers along the hospital corridors. The few words they exchange in these hours will create a very close link between the two, which by chance develops and complicates, and changes their lives in a decisive way.",
-        poster: "posters/parallelMothers.jpg",
-        trailer: undefined,
-        showtimes: {
-            mon: ["7:30pm"],
-            tue: ["7:30pm"],
-            wed: ["7:30pm"],
-            thu: ["7:30pm"],
-            fri: ["5:10pm", "7:30pm"],
-            sat: ["12:30pm", "2:30pm", "5:10pm", "7:30pm"],
-            sun: ["12:30pm", "2:30pm", "5:10pm", "7:30pm"],
-        },
-    },
-    movie4: {
-        title: "Licorice Pizza",
-        synopsis:
-            "Alana Kane and Gary Valentine grow up, run around and fall in love in California's San Fernando Valley in the 1970s.",
-        poster: "posters/licoricePizza.jpg",
-        trailer: undefined,
-        showtimes: {
-            mon: [""],
-            tue: ["7:30pm"],
-            wed: [""],
-            thu: ["7:30pm"],
-            fri: ["7:30pm"],
-            sat: ["12:30pm", "5:10pm"],
-            sun: ["12:30pm", "5:10pm"],
-        },
-    },
-
-    movie5: {
-        title: "West Side Story",
-        synopsis:
-            "Love at first sight strikes when young Tony spots Maria at a high school dance in 1957 New York City. Their burgeoning romance helps to fuel the fire between the warring Jets and Sharks -- two rival gangs vying for control of the streets.",
-        poster: "posters/WSSposter.jpg",
-        trailer: undefined,
-        showtimes: {
-            mon: ["7:30pm"],
-            tue: [""],
-            wed: ["7:30pm"],
-            thu: [""],
-            fri: ["5:10pm"],
-            sat: ["2:30pm", "7:30pm"],
-            sun: ["2:30pm", "7:30pm"],
-        },
-    },
-};
+console.log(d.getDate());
+console.log(d.toString().split(" ")[1], d.toString().split(" ")[2]);
 
 let friday = document.querySelector("[data-fri]");
 let saturday = document.querySelector("[data-sat]");
@@ -215,7 +135,7 @@ thursday.addEventListener("click", () => {
 
 switch (currentDay) {
     case 0:
-        sunday.classList.add("--is-active");
+        sunday.setAttribute("data-active", "true");
         // loop through the movie objects in schedule object
         for (let movie in schedule) {
             // if the movie object has show times for the specific day, create card ele + add showtimes
@@ -226,7 +146,7 @@ switch (currentDay) {
         break;
 
     case 1:
-        monday.classList.add("--is-active");
+        monday.setAttribute("data-active", "true");
         for (let movie in schedule) {
             if (schedule[movie].showtimes.mon[0] != "") {
                 createMovieCard(movie, "mon");
@@ -235,7 +155,7 @@ switch (currentDay) {
         break;
 
     case 2:
-        tuesday.classList.add("--is-active");
+        tuesday.setAttribute("data-active", "true");
         for (let movie in schedule) {
             if (schedule[movie].showtimes.tue[0] != "") {
                 createMovieCard(movie, "tue");
@@ -244,7 +164,7 @@ switch (currentDay) {
         break;
 
     case 3:
-        wednesday.classList.add("--is-active");
+        wednesday.setAttribute("data-active", "true");
         for (let movie in schedule) {
             if (schedule[movie].showtimes.wed[0] != "") {
                 createMovieCard(movie, "wed");
@@ -253,7 +173,7 @@ switch (currentDay) {
         break;
 
     case 4:
-        thursday.classList.add("--is-active");
+        thursday.setAttribute("data-active", "true");
         for (let movie in schedule) {
             if (schedule[movie].showtimes.thu[0] != "") {
                 createMovieCard(movie, "thu");
@@ -262,7 +182,7 @@ switch (currentDay) {
         break;
 
     case 5:
-        friday.classList.add("--is-active");
+        friday.setAttribute("data-active", "true");
         for (let movie in schedule) {
             if (schedule[movie].showtimes.fri[0] != "") {
                 createMovieCard(movie, "fri");
@@ -271,7 +191,7 @@ switch (currentDay) {
         break;
 
     case 6:
-        saturday.classList.add("--is-active");
+        saturday.setAttribute("data-active", "true");
         for (let movie in schedule) {
             if (schedule[movie].showtimes.sat[0] != "") {
                 createMovieCard(movie, "sat");
---
+++ js/schedule.js
@@ -0,0 +1,83 @@
+export let schedule = {
+    movie1: {
+        title: "Belfast",
+        synopsis:
+            "BELFAST is a movie straight from Branagh's own experience. A nine-year-old boy must chart a path towards adulthood through a world that has suddenly turned upside down. His stable and loving community and everything he thought he understood about life is changed forever but joy, laughter, music and the formative magic of the movies remain.",
+        poster: "posters/belfastPoster.jpg",
+        trailer: undefined,
+        showtimes: {
+            mon: ["7:30pm"],
+            tue: ["7:30pm"],
+            wed: ["7:30pm"],
+            thu: ["7:30pm"],
+            fri: ["5:10pm", "7:30pm"],
+            sat: ["12:30pm", "2:30pm", "5:10pm", "7:30pm"],
+            sun: ["12:30pm", "2:30pm", "5:10pm", "7:30pm"],
+        },
+    },
+    movie2: {
+        title: "House of Gucci",
+        synopsis:
+            "House of Gucci is inspired by the shocking true story of the family behind the Italian fashion empire. When Patrizia Reggiani (Lady Gaga), an outsider from humble beginnings, marries into the Gucci family, her unbridled ambition begins to unravel the family legacy and triggers a reckless spiral of betrayal, decadence, revenge, and ultimately... murder.",
+        poster: "posters/gucciPoster.jpg",
+        trailer: "https://www.youtube.com/watch?v=pGi3Bgn7U5U",
+        showtimes: {
+            mon: ["7:30pm"],
+            tue: ["7:30pm"],
+            wed: ["7:30pm"],
+            thu: ["7:30pm"],
+            fri: ["5:10pm", "7:30pm"],
+            sat: ["12:30pm", "2:30pm", "5:10pm", "7:30pm"],
+            sun: ["12:30pm", "2:30pm", "5:10pm", "7:30pm"],
+        },
+    },
+    movie3: {
+        title: "Parallel Mothers",
+        synopsis:
+            "Two women, Janis and Ana, coincide in a hospital room where they are going to give birth. Both are single and became pregnant by accident. Janis, middle-aged, doesn't regret it and she is exultant. The other, Ana, an adolescent, is scared, repentant and traumatized. Janis tries to encourage her while they move like sleepwalkers along the hospital corridors. The few words they exchange in these hours will create a very close link between the two, which by chance develops and complicates, and changes their lives in a decisive way.",
+        poster: "posters/parallelMothers.jpg",
+        trailer: undefined,
+        showtimes: {
+            mon: ["7:30pm"],
+            tue: ["7:30pm"],
+            wed: ["7:30pm"],
+            thu: ["7:30pm"],
+            fri: ["5:10pm", "7:30pm"],
+            sat: ["12:30pm", "2:30pm", "5:10pm", "7:30pm"],
+            sun: ["12:30pm", "2:30pm", "5:10pm", "7:30pm"],
+        },
+    },
+    movie4: {
+        title: "Licorice Pizza",
+        synopsis:
+            "Alana Kane and Gary Valentine grow up, run around and fall in love in California's San Fernando Valley in the 1970s.",
+        poster: "posters/licoricePizza.jpg",
+        trailer: undefined,
+        showtimes: {
+            mon: [""],
+            tue: ["7:30pm"],
+            wed: [""],
+            thu: ["7:30pm"],
+            fri: ["7:30pm"],
+            sat: ["12:30pm", "5:10pm"],
+            sun: ["12:30pm", "5:10pm"],
+        },
+    },
+
+    movie5: {
+        title: "West Side Story",
+        synopsis:
+            "Love at first sight strikes when young Tony spots Maria at a high school dance in 1957 New York City. Their burgeoning romance helps to fuel the fire between the warring Jets and Sharks -- two rival gangs vying for control of the streets.",
+        poster: "posters/WSSposter.jpg",
+        trailer: undefined,
+        showtimes: {
+            mon: ["7:30pm"],
+            tue: [""],
+            wed: ["7:30pm"],
+            thu: [""],
+            fri: ["5:10pm"],
+            sat: ["2:30pm", "7:30pm"],
+            sun: ["2:30pm", "7:30pm"],
+        },
+    },
+};
