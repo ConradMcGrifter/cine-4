2d90276035410c77cc9fe69e296d45604c61c155
skf <79630413+ConradMcGrifter@users.noreply.github.com>
2022-02-10 21:20:59 -0500

    added table of contents for js

---
+++ js/TABLE OF CONTENTS.md
@@ -0,0 +1,13 @@
+# FUNCTIONS
+
+-   createMovieCard.............this function takes parameters from schedule.js and creates the card element + appends to DOM
+
+-   removeCards...................removes all the cards currently in the DOM
+
+-   setTrailerSource..............gets all the card trailer elements and adds an event listener "on click" -> adds the data-trailer value to the source of the lightbox iframe
+
+-   displayShows................this function uses the setTrailerSource, createMovieCard, and removeCards functions. it takes a parameter to access a specific movie and day in the schedule.js object
+
+-   getDates......................function used to get a range of dates between two dates
+
+# TABS
--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 import { schedule } from "/js/schedule.js";
-import * as functions from "/js/functions/index.js";
+import { displayShows, getDates } from "./functions/index.js";
 import * as tabJS from "/js/tabs/index.js";
 
 tabJS.setCurrentTab(tabJS.currentDay); // set the active tab based on the current day
@@ -7,7 +7,7 @@ tabJS.activeTab(); // figure out which days have already passed and make them un
 // --------------------------------------------------
 
 // set the start and end date for the week of showtimes
-let dateRange = functions.getDates(new Date(2022, 1, 4), new Date(2022, 1, 10));
+let dateRange = getDates(new Date(2022, 1, 4), new Date(2022, 1, 10));
 // get all the date elements from the DOM
 let dates = document.querySelectorAll("[data-date]");
 // loop through each date and change the innerText to the month and day from the dateRange array
@@ -19,29 +19,29 @@ for (let i = 0; i < dates.length; i++) {
 
 // ------- Create card elements when specific tab is clicked on --------------
 tabJS.days.friday.addEventListener("click", () => {
-    functions.displayShows("fri");
+    displayShows("fri");
 });
 
 tabJS.days.saturday.addEventListener("click", () => {
-    functions.displayShows("sat");
+    displayShows("sat");
 });
 
 tabJS.days.sunday.addEventListener("click", () => {
-    functions.displayShows("sun");
+    displayShows("sun");
 });
 
 tabJS.days.monday.addEventListener("click", () => {
-    functions.displayShows("mon");
+    displayShows("mon");
 });
 
 tabJS.days.tuesday.addEventListener("click", () => {
-    functions.displayShows("tue");
+    displayShows("tue");
 });
 
 tabJS.days.wednesday.addEventListener("click", () => {
-    functions.displayShows("wed");
+    displayShows("wed");
 });
 
 tabJS.days.thursday.addEventListener("click", () => {
-    functions.displayShows("thu");
+    displayShows("thu");
 });
