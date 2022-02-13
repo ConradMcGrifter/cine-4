af2b6faf00b3c0262d567e1c350b56bef90589ef
skf <79630413+ConradMcGrifter@users.noreply.github.com>
2022-02-12 23:11:31 -0500

    test

--- js/main.js
+++ js/main.js
@@ -47,50 +47,50 @@ switch (currentDay) {
     case 0:
         sunday.setAttribute("data-active", "true");
         days.sunday.querySelector(".tab__day").innerText = "TODAY";
+        resetHeight();
         displayShows("sun");
-        // resetHeight();
         break;
 
     case 1:
         monday.setAttribute("data-active", "true");
         monday.querySelector(".tab__day").innerText = "TODAY";
+        resetHeight();
         displayShows("mon");
-        // resetHeight();
         break;
 
     case 2:
         tuesday.setAttribute("data-active", "true");
         tuesday.querySelector(".tab__day").innerText = "TODAY";
+        resetHeight();
         displayShows("tue");
-        // resetHeight();
         break;
 
     case 3:
         wednesday.setAttribute("data-active", "true");
         wednesday.querySelector(".tab__day").innerText = "TODAY";
+        resetHeight();
         displayShows("wed");
-        // resetHeight();
         break;
 
     case 4:
         thursday.setAttribute("data-active", "true");
         thursday.querySelector(".tab__day").innerText = "TODAY";
+        resetHeight();
         displayShows("thu");
-        // resetHeight();
         break;
 
     case 5:
         friday.setAttribute("data-active", "true");
         friday.querySelector(".tab__day").innerText = "TODAY";
+        resetHeight();
         displayShows("fri");
-        // resetHeight();
         break;
 
     case 6:
         saturday.setAttribute("data-active", "true");
         saturday.querySelector(".tab__day").innerText = "TODAY";
+        resetHeight();
         displayShows("sat");
-        // resetHeight();
         break;
 }
 // --------------------------------------------------------------------------
