9640b62598c56b48741b7e1bb8e7b22dd5049799
skf <79630413+ConradMcGrifter@users.noreply.github.com>
2022-02-24 18:08:41 -0500

    started working on rating styles for movies

--- js/schedule.js
+++ js/schedule.js
@@ -5,6 +5,7 @@ if a movie doesn't have a showtime on a certain day, add an empty string into th
 export let schedule = {
     movie1: {
         title: "Death on the Nile",
+        rating: "PG-13",
         synopsis:
             "Belgian sleuth Hercule Poirot's Egyptian vacation aboard a glamorous river steamer turns into a terrifying search for a murderer when a picture-perfect couple's idyllic honeymoon is tragically cut short. Set against an epic landscape of sweeping desert vistas and the majestic Giza pyramids, this tale of unbridled passion and incapacitating jealousy features a cosmopolitan group of impeccably dressed travelers, and enough wicked twists and turns to leave audiences guessing until the final, shocking denouement.",
         poster: "posters/deathOnTheNile.jpg",
@@ -22,6 +23,7 @@ export let schedule = {
 
     movie2: {
         title: "Jockey",
+        rating: "R",
         synopsis:
             "An aging jockey (Clifton Collins Jr.), hopes to win one last title for his longtime trainer (Molly Parker), who has acquired what appears to be a championship horse. But the years -- and injuries -- have taken a toll on his body, throwing into question his ability to continue his lifelong passion. And the arrival of a young rookie rider (Moises Arias), who claims to be his son, and whom he takes under his wing, further complicates the path to fulfilling his dream.",
         poster: "posters/jockey.jpg",
@@ -39,6 +41,7 @@ export let schedule = {
 
     movie3: {
         title: "Belfast",
+        rating: "R",
         synopsis:
             "BELFAST is a movie straight from Branagh's own experience. A nine-year-old boy must chart a path towards adulthood through a world that has suddenly turned upside down. His stable and loving community and everything he thought he understood about life is changed forever but joy, laughter, music and the formative magic of the movies remain.",
         poster: "posters/belfastPoster.jpg",
@@ -55,6 +58,7 @@ export let schedule = {
     },
     movie4: {
         title: "Parallel Mothers",
+        rating: "R",
         synopsis:
             "Two women, Janis and Ana, coincide in a hospital room where they are going to give birth. Both are single and became pregnant by accident. Janis, middle-aged, doesn't regret it and she is exultant. The other, Ana, an adolescent, is scared, repentant and traumatized. Janis tries to encourage her while they move like sleepwalkers along the hospital corridors. The few words they exchange in these hours will create a very close link between the two, which by chance develops and complicates, and changes their lives in a decisive way.",
         poster: "posters/parallelMothers.jpg",
@@ -72,6 +76,7 @@ export let schedule = {
 
     movie5: {
         title: "Licorice Pizza",
+        rating: "R",
         synopsis:
             "Alana Kane and Gary Valentine grow up, run around and fall in love in California's San Fernando Valley in the 1970s.",
         poster: "posters/licoricePizza.jpg",
@@ -89,6 +94,7 @@ export let schedule = {
 
     movie6: {
         title: "West Side Story",
+        rating: "PG-13",
         synopsis:
             "Love at first sight strikes when young Tony spots Maria at a high school dance in 1957 New York City. Their burgeoning romance helps to fuel the fire between the warring Jets and Sharks -- two rival gangs vying for control of the streets.",
         poster: "posters/WSSposter.jpg",
