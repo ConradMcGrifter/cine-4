f2aa13fec92e91d628572136cfbbe63af8d81f91
skf <79630413+ConradMcGrifter@users.noreply.github.com>
2022-02-26 22:35:29 -0500

    fixed scroll behavior with tabs

--- js/functions/display/displayCurrentDay.js
+++ js/functions/display/displayCurrentDay.js
@@ -19,49 +19,49 @@ export function displayCurrentDay(day) {
         case 0:
             sunday.setAttribute("data-active", "true");
             sunday.querySelector(".tab__day").innerText = "TODAY";
-            sunday.scrollIntoView();
+            document.querySelector("[data-tabs-wrap]").scroll(0, 0);
             displayShows("sun", schedule);
             break;
 
         case 1:
             monday.setAttribute("data-active", "true");
             monday.querySelector(".tab__day").innerText = "TODAY";
-            monday.scrollIntoView();
+            document.querySelector("[data-tabs-wrap]").scroll(0, 0);
             displayShows("mon", schedule);
             break;
 
         case 2:
             tuesday.setAttribute("data-active", "true");
             tuesday.querySelector(".tab__day").innerText = "TODAY";
-            tuesday.scrollIntoView();
+            document.querySelector("[data-tabs-wrap]").scroll(0, 0);
             displayShows("tue", schedule);
             break;
 
         case 3:
             wednesday.setAttribute("data-active", "true");
             wednesday.querySelector(".tab__day").innerText = "TODAY";
-            wednesday.scrollIntoView();
+            document.querySelector("[data-tabs-wrap]").scroll(0, 0);
             displayShows("wed", schedule);
             break;
 
         case 4:
             thursday.setAttribute("data-active", "true");
             thursday.querySelector(".tab__day").innerText = "TODAY";
-            thursday.scrollIntoView();
+            document.querySelector("[data-tabs-wrap]").scroll(0, 0);
             displayShows("thu", schedule);
             break;
 
         case 5:
             friday.setAttribute("data-active", "true");
             friday.querySelector(".tab__day").innerText = "TODAY";
-            friday.scrollIntoView();
+            document.querySelector("[data-tabs-wrap]").scroll(0, 0);
             displayShows("fri", schedule);
             break;
 
         case 6:
             saturday.setAttribute("data-active", "true");
             saturday.querySelector(".tab__day").innerText = "TODAY";
-            saturday.scrollIntoView();
+            document.querySelector("[data-tabs-wrap]").scroll(0, 0);
             displayShows("sat", schedule);
             break;
     }
